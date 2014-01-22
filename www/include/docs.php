4892bffd0660676bc61f2652553ea200b2f953ef
tmont <tommy.mont@gmail.com>
2014-01-22 00:23:16 -0800

    deleted www directory

--- www/include/docs.php
+++
@@ -1,8 +0,0 @@
-					<h2>Documentation</h2>
-					<div class="inset">
-						<ul>
-							<li><a href="/docs/api/">API Documentation</a></li>
-							<li><a href="/docs/coverage/">Code Coverage</a></li>
-							<li><a href="/docs/pdepend">PHP Depend</a></li>
-						</ul>
-					</div>
--- www/include/resources.php
+++
@@ -1,7 +0,0 @@
-					<h2>Resources</h2>
-					<div class="inset">
-						<ul>
-							<li><a href="http://www.sonicspot.com/guide/midifiles.html">MIDI File Format - The Sonic Spot</a></li>
-							<li><a href="http://www.midi.org/techspecs/midispec.php">Official Spec</a></li>
-						</ul>
-					</div>
